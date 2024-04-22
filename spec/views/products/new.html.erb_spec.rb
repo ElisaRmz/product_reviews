36628bf27b2303991b0a805e3a0cc90b2c8f625d
EliRmz <elisaram@gmail.com>
2024-04-22 16:35:06 +0200

    Add Product main structure

---
+++ spec/views/products/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "products/new", type: :view do
+  before(:each) do
+    assign(:product, Product.new(
+      name: "MyString",
+      url: "MyString",
+      description: "MyText"
+    ))
+  end
+
+  it "renders new product form" do
+    render
+
+    assert_select "form[action=?][method=?]", products_path, "post" do
+
+      assert_select "input[name=?]", "product[name]"
+
+      assert_select "input[name=?]", "product[url]"
+
+      assert_select "textarea[name=?]", "product[description]"
+    end
+  end
+end
