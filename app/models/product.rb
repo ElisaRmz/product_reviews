4c8985c62ce26b01e05a04779c1fd387cd7c8bfb
EliRmz <elisaram@gmail.com>
2024-04-23 16:33:34 +0200

    Add url validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   belongs_to :user
+
+  validates :url, format: { with: URI::regexp(%w[http https]), message: "must be a valid url" }
+
 end
